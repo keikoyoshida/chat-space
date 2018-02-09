52f0e1411314d7f3ddb8e655d4aab879ad624932
Keiko Yoshida <kitchen8nn@gmail.com>
2018-02-09 13:32:14 -0800

    add unique constraint to email column

---
+++ db/migrate/20180209204257_change_column_to_user.rb
@@ -0,0 +1,14 @@
+class ChangeColumnToUser < ActiveRecord::Migration[5.0]
+  def change
+    # 変更内容
+  def up
+    change_column :users, :email, :string, null: false, default: "nil"
+    add_index :users, :email, :unique => true
+  end
+
+  # 変更前の状態
+  def down
+    change_column :users, :email, :string, null: false, default: ""
+  end
+  end
+end
