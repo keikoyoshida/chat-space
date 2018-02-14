4170ccd0cdee989accc2afa6623111f87565c41b
Keiko Yoshida <kitchen8nn@gmail.com>
2018-02-14 14:15:25 -0800

    modify association between group model and group_user model

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,6 +3,6 @@ class Group < ApplicationRecord
 
   has_many :users, through: :group_user
   has_many :group_users
-  accepts_nested_attributes_for :group_user
+  accepts_nested_attributes_for :group_users
   # has_many :messages
 end
