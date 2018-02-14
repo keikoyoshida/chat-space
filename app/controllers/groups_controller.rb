3ccb40b30fd389b9f26c268f1de683f0852bbf76
Keiko Yoshida <kitchen8nn@gmail.com>
2018-02-14 11:54:10 -0800

    set groups new action

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,20 @@
 class GroupsController < ApplicationController
+  def new
+  end
+
+  # def create
+  #   # Group.create(group_params)
+  # end
+
+  # def edit
+  # end
+
+  # def update
+  # end
+
+  # private
+
+  # def group_params
+  #   params.require(:group).permit(:name, group_users_attributes: [:user_id])
+  # end
 end
