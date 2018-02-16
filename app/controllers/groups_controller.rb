37a610c6e4494ece989e0c63a08eea9ccd584965
Keiko Yoshida <kitchen8nn@gmail.com>
2018-02-15 23:37:50 -0800

    Define new and create action in groups controller
    new
    - add current_user to new group instance
    create
    - if new group instance can be saved, redirect_to root_path and show
    the notice flash.
    - if it can’t be, new action view is shown again. The error message is
    written with error message object in the new view file.

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,11 +1,17 @@
 class GroupsController < ApplicationController
   def new
     @group = Group.new
+    @group.users << current_user
   end
 
-  # def create
-  #   # Group.create(group_params)
-  # end
+  def create
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to root_path, notice: 'グループを作成しました'
+    else
+      render :new
+    end
+  end
 
   # def edit
   # end
@@ -13,9 +19,9 @@ class GroupsController < ApplicationController
   # def update
   # end
 
-  # private
+  private
 
-  # def group_params
-  #   params.require(:group).permit(:name, group_users_attributes: [:user_id])
-  # end
+  def group_params
+    params.require(:group).permit(:name, { :user_ids => [] })
+  end
 end
