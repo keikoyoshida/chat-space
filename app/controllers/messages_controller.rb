5a18e9dbb974d07a5beea9fb8d64b52df8b221d8
Keiko Yoshida <kitchen8nn@gmail.com>
2018-02-17 00:13:00 -0800

    define messages#index action

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,17 @@
 class MessagesController < ApplicationController
+  before_action :set_group
+
   def index
+    @messages = Message.where(group_id: params[:id])
+    @message = Message.new
+  end
+
+  def create
+  end
+
+  private
+
+  def set_group
+    @group = Group.find(params[:group_id])
   end
 end
