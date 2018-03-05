545737f3efd7db4b48fd3f16a3d41a3543538841
Keiko Yoshida <kitchen8nn@gmail.com>
2018-03-05 13:31:34 -0800

    refactor code
    - put out this application’s function from ready method
    - avoid clicks triggers event too many times

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,29 +1,5 @@
 $(function () {
 
-function appendUser(user) {
-  var html = `<div class="chat-group-user clearfix">
-                <p class="chat-group-user__name">${ user.name }</p>
-                <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${ user.id }" data-user-name="${ user.name }">追加</a>
-              </div>`
-  $("#user-search-result").append(html);
-}
-
-function appendNoUser(user) {
-  var html =`<div class="chat-group-user clearfix">
-              ${ user }
-             </div>`
-  $("#user-search-result").append(html);
-}
-
-function appendGroupUser(user) {
-  var html =`<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-${ user.userId }'>
-              <input name='group[user_ids][]' type='hidden' value='${ user.userId }'>
-              <p class='chat-group-user__name'>${ user.userName }</p>
-              <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
-            </div>`
-  $("#group-user").append(html);
-}
-
   $('#user-search-field').on("keyup", function() {
     var input = $(this).val();
     $.ajax({
@@ -48,14 +24,38 @@ function appendGroupUser(user) {
     });
   });
 
-  $(document).on("click", ".user-search-add", function(){
+  $('#user-search-result').on("click", ".user-search-add", function(){
     $("#user-search-result").empty();
     var user = $(this).data();
     appendGroupUser(user);
   });
 
-  $(document).on("click", ".user-search-remove", function(){
+  $('#group-user').on("click", ".user-search-remove", function(){
     $(this).parent().remove();
   });
 
 });
+
+function appendUser(user) {
+  var html = `<div class="chat-group-user clearfix">
+                <p class="chat-group-user__name">${ user.name }</p>
+                <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${ user.id }" data-user-name="${ user.name }">追加</a>
+              </div>`
+  $("#user-search-result").append(html);
+}
+
+function appendNoUser(user) {
+  var html =`<div class="chat-group-user clearfix">
+              ${ user }
+             </div>`
+  $("#user-search-result").append(html);
+}
+
+function appendGroupUser(user) {
+  var html =`<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-${ user.userId }'>
+              <input name='group[user_ids][]' type='hidden' value='${ user.userId }'>
+              <p class='chat-group-user__name'>${ user.userName }</p>
+              <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
+            </div>`
+  $("#group-user").append(html);
+}
