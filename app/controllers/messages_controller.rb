8e6dd02a2bfe8fd1b7c658e85778e89143c5f75f
Keiko Yoshida <kitchen8nn@gmail.com>
2018-03-08 16:18:25 -0800

    - change messages#index controller
    When moving page or reloading, @messages has whole message in the group.
    When screen transition, messages whose id is over than the showing last
    message.

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,7 +3,11 @@ class MessagesController < ApplicationController
 
   def index
     @message = Message.new
-    @messages = @group.messages.includes(:user)
+    if params[:lastMessageId].present?
+      @messages = @group.messages.where("id > ?", params[:lastMessageId]).includes(:user)
+    else
+      @messages = @group.messages.includes(:user)
+    end
     respond_to do |format|
         format.html
         format.json
